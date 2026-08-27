10e207ee923565707eefdbe560ab677aaeed2efa
Reid Cameron <reidwesleycameron@gmail.com>
2026-08-27 12:48:40 -0500

    added my mark

--- hello.c
+++ hello.c
@@ -9,5 +9,6 @@ int main(int argc, char **argv) {
   printf("Hello, I'm Jose Lopez!\n");
   printf("My Major is Computer Engineering!\n");
   printf("Minor will be French\n");
+  printf("reid cameron on the track");
    return 0;
 }
