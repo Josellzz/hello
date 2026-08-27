1a7d0c315e51f59bbea9096c4a22355b08e10b0c
Jose Lopez <josellzzthe@gmail.com>
2026-08-27 00:20:33 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argc) {
+
+  printf("Hello, I'm Jose Lopez!/n");
+  printf("My Major is Computer Engineering!/n");
+
+   return 0;
+}
