547b0f07c996f12cb4f9f8f2634852d7467eb8c6
Jose Lopez <josellzzthe@gmail.com>
2026-08-27 01:36:56 -0500

    fixed standard library bugg

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 /**
  * Basic Hello World Program
  */
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char **argc) {
+int main(int argc, char **argv) {
 
-  printf("Hello, I'm Jose Lopez!/n");
-  printf("My Major is Computer Engineering!/n");
+  printf("Hello, I'm Jose Lopez!\n");
+  printf("My Major is Computer Engineering!\n");
 
    return 0;
 }
